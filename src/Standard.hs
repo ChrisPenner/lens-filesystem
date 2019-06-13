8c59472fd434c0f8acd217dd01ac001d7eff9ad6
Chris Penner <christopher.penner@gmail.com>
2019-06-14 01:18:14 +0200

    Add crawling helpers

--- src/Standard.hs
+++ src/Standard.hs
@@ -4,15 +4,19 @@
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE TemplateHaskell #-}
 module Standard where
 
 import Control.Lens
 import Control.Lens.Action
 import Control.Lens.Action.Internal
+import Control.Lens.Internal.Bazaar
 import Control.Lens.Action.Type
 import System.Directory
 import System.FilePath.Posix
 import Data.Functor
+import Control.Monad
+import Control.Applicative
 
 -- data File = File{ filePath :: FilePath, contents :: String }
 data FileType = Dir | File | SymLink
@@ -31,6 +35,7 @@ file filePath = to (</> filePath)
 dir :: FilePath -> Getter FilePath FilePath
 dir = file
 
+-- ls :: MonadicFold IO FilePath [FilePath]
 ls :: MonadicFold IO FilePath [FilePath]
 ls = act listDirectory
 
@@ -38,17 +43,21 @@ mv :: FilePath -> MonadicFold IO FilePath ()
 mv filePath = act (flip renamePath filePath)
 
 
+infixr 8 !%=
+(!%=) :: (Monad m) => Acting m b s a -> (a -> m b) -> s -> m b
+(!%=) action f = perform (action . act f)
+
+infixr 8 !!%=
+(!!%=) :: (Monad m) => Acting m [b] s a -> (a -> m b) -> s -> m [b]
+action !!%= f = (^!! action . act f)
+
 infixr 8 !%~
-(!%~) :: MonadicFold m s a -> (a -> m b) -> MonadicFold m s b
-action !%~ f = action . act f
+(!%~) :: (Monad m) => Acting m b s a -> (a -> b) -> s -> m b
+(!%~) action f = perform (action . to f)
 
--- infixr 8 &!
-(&!) :: (Monoid b, Monad m) => s -> MonadicFold m s b -> m b
-v &! l = v ^! l
-
-infixr 8 &!!
-(&!!) :: (Monad m) => s -> MonadicFold m s b -> m [b]
-v &!! l = v ^!! l
+infixr 8 !!%~
+(!!%~) :: (Monad m) => Acting m [b] s a -> (a -> b) -> s -> m [b]
+action !!%~ f = (^!! action . to f)
 
 -- filteredM :: (Choice p, Applicative f, Effective m r f) => (a -> m Bool) -> Optic' p f a a
 --  -- Optic p f s t a b = p a (f a) -> p s (f s)
@@ -77,13 +86,49 @@ v &!! l = v ^!! l
 --         if b then ineffective $ f a
 --              else pure a
 
+-- | 'try' will run a given monadic fold, recovering on failure
+-- TODO: generalize this to not need monoid
+--
+-- try :: (Alternative m, Monoid r, Effective m r f) => ((a -> f a) -> s -> f s) -> ((a -> f a) -> s -> f s)
+try :: (Monad m, Alternative m, Monoid r) => Acting m r s a -> Acting m r s a
+try fld f s = effective (ineffective (fld f s) <|> pure mempty)
+
+-- |
+iterating :: (Monad m, Alternative m) => Acting m r a a -> Acting m r a a
+iterating fld f a = effective (ineffective (fld f a) <|> ineffective (f a))
+
+-- filteredM :: (Monad m, Monoid r) => (a -> m Bool) -> Acting m r a a
+filteredM predicate f a = effective go
+  where
+    go = do
+      predicate a >>= \case
+        True -> ineffective (f a)
+        False -> pure mempty
+
+crawl :: Monoid r => Acting IO r FilePath FilePath
+crawl = iterating (ls . traversed . crawl)
+
+data Config = Config
+    { _workDir :: FilePath
+    }
+
+makeLenses ''Config
+
 
 main :: IO ()
 main = do
-    fileContents <- "." ^!! file "README.md" . contents . lined
+    -- fileContents <- "." ^!! file "README.md" . contents . lined
     -- print fileContents
     -- "." ^!! file "README.md" . contents . lined !%~ print
-    "." &!! file "README.md" . contents . lined !%~ pure
+    -- "." &! file "README.md" . contents . lined !%~ print
+    -- "." &! file "README.md" !%~ flip renamePath "README2.md"
+    -- Config "." & workDir . file "README.md" !%~ flip renamePath "README2.md"
+    -- Config "." & workDir . file "README2.md" !%~ flip renamePath "README.md"
+    -- r <- Config "." & workDir . file "README.md" !%~ pure
+    -- r <- Config "." & workDir . ls . traversed !!%= pure
+    -- r <- Config "." & workDir . ls . traversed . try ls . traversed !!%~ id
+    r <- Config "." ^! workDir . ls . traversed . filteredM doesDirectoryExist . act print
+    print r
     -- dirContents <- "." ^!! ls . traversed . filteredM doesDirectoryExist
     -- print dirContents
     return ()
