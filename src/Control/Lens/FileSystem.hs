962d7adc146a03aa7f9135d46e3a1aa8f0e1be3c
Chris Penner <christopher.penner@gmail.com>
2019-06-21 11:04:39 +0200

    Add some haddocks

--- src/Control/Lens/FileSystem.hs
+++ src/Control/Lens/FileSystem.hs
@@ -6,7 +6,9 @@
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE FlexibleContexts #-}
 module Control.Lens.FileSystem
-    ( ls
+    (
+    -- * File System Helpers
+      ls
     , ls'ed
     , path
     , pathL
@@ -20,18 +22,23 @@ module Control.Lens.FileSystem
     , absolute
     , withPerms
     , symLinksFollowed
-    -- , localized
 
     -- * Combinators
-    , recovering
-    , tryOrContinue
-    , tryCatch
     , filteredM
     , merging
     , including
 
+    -- ** Exception Handling
+    , recovering
+    , tryOrContinue
+    , tryCatch
+
+    -- * Re-exports
     , (</>)
 
+    , readable
+    , writable
+    , executable
     , module System.FilePath.Lens
     ) where
 
@@ -41,50 +48,79 @@ import Control.Lens.FileSystem.Combinators
 import System.Directory
 import System.FilePath.Posix
 import System.FilePath.Lens
-import qualified Data.Text as T
-import qualified Data.Text.IO as T
 
-ls :: Acting IO r FilePath [FilePath]
-ls = act (\fp -> (fmap (fp </>)) <$> listDirectory fp)
+-- | List the files at a given directory
+-- If the focused path isn't a directory this fold will return 0 results
+ls :: Monoid r => Acting IO r FilePath [FilePath]
+ls = recovering $ act (\fp -> (fmap (fp </>)) <$> listDirectory fp)
 
+-- | Fold over all files in the given directory.
+-- If the focused path isn't a directory this fold will return 0 results
+-- This is an alias for @@ls . traversed@@
 ls'ed :: Monoid r => Acting IO r FilePath FilePath
 ls'ed = ls . traversed
 
+
+-- | Append a path the end of the current path.
+-- This uses `</>` for cross platform compatibility so
+-- you don't need leading/trailing slashes here
 path :: FilePath -> Getter FilePath FilePath
 path filePath = to (</> filePath)
 
+-- | Create a filepath from a list of path segments, then append it to the focused path.
+-- @pathL ["a", "b"] == path "a" . path "b" == to (</> "a/b")@
 pathL :: [FilePath] -> Getter FilePath FilePath
 pathL filePaths = to (</> joinPath filePaths)
 
+-- | "Branch" a fold into many sub-paths.
+-- E.g. if we want to crawl into BOTH of @src@ and @test@ directories we can do:
+--
+-- > "." ^! branching ["src", "test"] . crawled
 branching :: [FilePath] -> Fold FilePath FilePath
 branching filePaths = folding (\fp -> (fp </>) <$> filePaths)
 
+-- | Filter for only paths which point to a valid directory
 dirs :: (Monoid r) => Acting IO r FilePath FilePath
 dirs = filteredM doesDirectoryExist
 
+-- | Filter for only paths which point to a valid file
 files :: (Monoid r) => Acting IO r FilePath FilePath
 files = filteredM doesFileExist
 
-contents :: Action IO FilePath String
-contents = act readFile
+-- | Get the contents of a file
+-- This fold will return 0 results if the path does not exist, if it isn't a file, or if
+-- reading the file causes any exceptions.
+--
+-- This fold lifts the path of the current file into the index of the fold in case you need it
+-- downstream.
+contents :: (Monoid r) => Acting IO r FilePath String
+contents = selfIndex . recovering (act readFile)
 
-exts :: Monoid r => [String] -> Acting IO r FilePath FilePath
+-- | Filter the fold for only files which have ANY of the given file extensions.
+-- E.g. to find all Haskell or Markdown files in the current directory:
+--
+-- > "." ^! crawled . exts ["hs", "md"]
+-- exts :: Monoid r => [String] -> Acting IO r FilePath FilePath
+exts :: [String] -> Traversal' FilePath FilePath
 exts extList = filtered check
   where
     check fp = drop 1 (takeExtension fp) `elem` extList
 
+-- | Crawl over every file AND directory in the given path.
 crawled :: Monoid r => Acting IO r FilePath FilePath
 crawled = including (dirs . ls . traversed . crawled)
 
 -- continually run the given fold until all branches hit dead ends,
--- folding all elements along the way.
--- TODO: maybe add 'recovering'?
+-- yielding over all elements encountered the way.
 crawling :: Monoid r => Acting IO r FilePath FilePath -> Acting IO r FilePath FilePath
-crawling fld = including (fld . crawling fld)
+crawling fld = including (recovering (fld . crawling fld))
 
+-- | Make filepaths absolute in reference to the current working directory
 absolute :: MonadicFold IO FilePath FilePath
 absolute = act makeAbsolute
 
+-- | Filter for only paths which have ALL of the given file-permissions
+-- See 'readable', 'writable', 'executable'
 withPerms :: Monoid r => [Permissions -> Bool] -> Acting IO r FilePath FilePath
 withPerms permChecks = filteredM checkAll
   where
@@ -92,8 +128,7 @@ withPerms permChecks = filteredM checkAll
         perms <- getPermissions fp
         return $ all ($ perms) permChecks
 
+-- | If the path is a symlink, rewrite the path to its destination and keep folding
+-- If it's not a symlink; pass the path onwards as is.
 symLinksFollowed :: Monoid r => Acting IO r FilePath FilePath
-symLinksFollowed = tryCatch (act getSymbolicLinkTarget) pure
-
-localized :: (Conjoined p, Effective IO r f) => IO a -> Optic' p f FilePath a
-localized action = act (flip withCurrentDirectory action)
+symLinksFollowed = tryOrContinue (act getSymbolicLinkTarget)
--- src/Control/Lens/FileSystem/Combinators.hs
+++ src/Control/Lens/FileSystem/Combinators.hs
@@ -7,19 +7,21 @@ import Control.Lens.Action
 import Control.Lens.Action.Internal
 import Control.Applicative
 
--- | 'recovering' will run a given monadic fold, recovering with an empty fold on failure
---
--- try :: (Alternative m, Monoid r, Effective m r f) => ((a -> f a) -> s -> f s) -> ((a -> f a) -> s -> f s)
+-- | If a given fold fails (e.g. with an exception), recover and simply return 0 elements
+-- rather than crashing.
 recovering :: (Monad m, Alternative m, Monoid r) => Acting m r s a -> Acting m r s a
 recovering fld f s = effective (ineffective (fld f s) <|> pure mempty)
 
--- Tries the given fold, if it fails then return the input as output
+-- | Try the given fold, if it throws an exception then return the input as the output instead
 tryOrContinue :: (Monad m, Alternative m) => Acting m r a a -> Acting m r a a
 tryOrContinue = flip tryCatch pure
 
+-- | Try the given fold, if it throws an exception then use the given handler to compute a
+-- replacement value and continue with that.
 tryCatch :: (Monad m, Alternative m) => Acting m r s b -> (s -> m b) -> Acting m r s b
 tryCatch fld handler f a = effective (ineffective (fld f a) <|> (handler a >>= ineffective . f))
 
+-- | Filter a fold using a monadic action
 filteredM :: (Monad m, Monoid r) => (a -> m Bool) -> Acting m r a a
 filteredM predicate f a = effective go
   where
@@ -28,31 +30,10 @@ filteredM predicate f a = effective go
         True -> ineffective (f a)
         False -> pure mempty
 
-
+-- | Merge two folds
 merging :: (Applicative f, Contravariant f) => LensLike' f s a -> LensLike' f s a -> LensLike' f s a
 merging fold1 fold2 nextFold s = fold1 nextFold s *> fold2 nextFold s
 
+-- | Include the results of an additional fold alongside the original values
 including :: (Applicative f, Contravariant f) => LensLike' f a a -> LensLike' f a a
 including = merging id
-
-infixr 8 %!
--- type LensLike f s t a b = (a -> f b) -> s -> f t
--- (%!) :: (Monad m) => LensLike m s t a b -> (a -> m b) -> s -> m t
-(%!) :: (Monad m) => ((a -> m b) -> s -> m t) -> (a -> m b) -> s -> m t
-(%!) l f s = l f s
-
--- infixr 8 %!
--- (%!) :: (Monad m) => Acting m b s a -> (a -> m b) -> s -> m b
--- (%!) action f = perform (action . act f)
-
--- infixr 8 %!!
--- (%!!) :: (Monad m) => Acting m [b] s a -> (a -> m b) -> s -> m [b]
--- action %!! f = (^!! action . act f)
-
--- infixr 8 !%~
--- (!%~) :: (Monad m) => Acting m b s a -> (a -> b) -> s -> m b
--- (!%~) action f = perform (action . to f)
-
--- infixr 8 !!%~
--- (!!%~) :: (Monad m) => Acting m [b] s a -> (a -> b) -> s -> m [b]
--- action !!%~ f = (^!! action . to f)
